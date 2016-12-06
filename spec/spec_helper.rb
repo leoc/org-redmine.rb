87ee79424b26ef478863cbc740a58106fe35466b
Arthur Andersen <aandersen@velalu.qa>
2016-12-06 10:29:27 +0100

    Configure rspec and guard

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler'
+require 'highline/import'
+require 'commander'
+require 'redmine_api'
+
+$LOAD_PATH << '../lib'
+
+require 'org'
+require 'core_ext'
+require 'org-redmine'
