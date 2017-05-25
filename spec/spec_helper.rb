bf18e3b2b0c39286baa08df77516de6e1a4426c5
Arthur Andersen <aandersen@velalu.qa>
2017-05-25 17:17:20 +0200

    Run only focussed specs if `focus: true` found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,8 @@ $LOAD_PATH << '../lib'
 require 'org'
 require 'core_ext'
 require 'org-redmine'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
