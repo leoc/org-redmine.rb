49363f9302041edb342fb416980bd7d85528b832
Arthur Andersen <aandersen@velalu.qa>
2017-06-19 14:29:27 +0200

    Add `Org` timestamp formatting/parsing methods

--- lib/org.rb
+++ lib/org.rb
@@ -1,6 +1,60 @@
+require 'date'
+
 module Org
   TIMESTAMP_DAY_REGEX = /(Mon|Tue|Wed|Thu|Fri|Sat|Sun)/
-  TIMESTAMP_REGEX = /\[\d{4}-\d{2}-\d{2}( #{TIMESTAMP_DAY_REGEX.source})?( \d{1,2}:\d{2}(:\d{2})?)?\]/
+  TIMESTAMP_DATE_REGEX = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/
+  TIMESTAMP_TIME_REGEX = /(?<hours>\d{1,2}):(?<minutes>\d{2})(:(?<seconds>\d{2}))?/
+  TIMESTAMP_REGEX = /(\[|<)#{TIMESTAMP_DATE_REGEX.source}( #{TIMESTAMP_DAY_REGEX.source})?( #{TIMESTAMP_TIME_REGEX.source})?(\]|>)/
+  WEEKDAY_MAPPING = {
+    0 => 'Sun',
+    1 => 'Mon',
+    2 => 'Tue',
+    3 => 'Wed',
+    4 => 'Thu',
+    5 => 'Fri',
+    6 => 'Sat',
+    7 => 'Sun'
+  }
+
+  def self.timestamp_to_date(timestamp)
+    return if timestamp.nil?
+    match = timestamp.match(TIMESTAMP_REGEX)
+    Date.new(
+      match[:year].try(:to_i),
+      match[:month].try(:to_i),
+      match[:day].try(:to_i)
+    )
+  end
+
+  def self.timestamp_to_datetime(timestamp)
+    return if timestamp.nil?
+    match = timestamp.match(TIMESTAMP_REGEX)
+    Date.new(
+      match[:year].try(:to_i),
+      match[:month].try(:to_i),
+      match[:day].try(:to_i),
+      match[:hour].try(:to_i),
+      match[:minute].try(:to_i)
+    )
+  end
+
+  def self.format_timestamp(date)
+    return if date.nil?
+    year = date.year
+    month = date.month.to_s.rjust(2, '0')
+    day = date.day.to_s.rjust(2, '0')
+    weekday = WEEKDAY_MAPPING[date.wday]
+    case date
+    when DateTime
+      format_timestamp(date.to_time)
+    when Time
+      hour = date.hour.to_s.rjust(2, '0')
+      minute = date.min.to_s.rjust(2, '0')
+      "<#{year}-#{month}-#{day} #{weekday} #{hour}:#{minute}>"
+    when Date
+      "<#{year}-#{month}-#{day} #{weekday}>"
+    end
+  end
 end
 
 require 'org/helpers'
@@ -9,6 +63,7 @@ require 'org/position'
 require 'org/file'
 require 'org/properties'
 require 'org/property'
+require 'org/planning'
 require 'org/logbook'
 require 'org/clock'
 require 'org_redmine/issue'
--- spec/lib/org_spec.rb
+++ spec/lib/org_spec.rb
@@ -13,4 +13,27 @@ describe Org do
       expect(Org::TIMESTAMP_REGEX).to match('[2017-06-03 Sat 10:00:22]')
     end
   end
+
+  describe '::timestamp_to_date' do
+    it 'creates date' do
+      expect(Org::timestamp_to_date('<2017-06-03 Sat>'))
+        .to eq(Date.new(2017, 6, 3))
+    end
+  end
+
+  describe '::format_timestamp' do
+    let(:date) { Date.new(2017, 6, 3) }
+    let(:datetime) { DateTime.new(2017, 6, 3, 12, 0) }
+    let(:time) { Time.at(1497608953) }
+
+    it 'formats date' do
+      expect(Org::format_timestamp(date)).to eq('<2017-06-03 Sat>')
+    end
+    it 'formats datetime' do
+      expect(Org::format_timestamp(datetime)).to eq('<2017-06-03 Sat 12:00>')
+    end
+    it 'formats time' do
+      expect(Org::format_timestamp(time)).to eq('<2017-06-16 Fri 12:29>')
+    end
+  end
 end
